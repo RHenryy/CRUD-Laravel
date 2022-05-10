51553da7cfa6855889fe966fa182e028c8596a2b
RHenryy <97943998+RHenryy@users.noreply.github.com>
2022-05-11 00:07:56 +0200

    Update 2022_03_22_121930_create_orders_table.php

--- veville/database/migrations/2022_03_22_121930_create_orders_table.php
+++ veville/database/migrations/2022_03_22_121930_create_orders_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id_order', 3);
-            $table->foreignId('id_member', 3)->references('id_member')->on('members')->onDelete('cascade');;
-            $table->foreignId('id_vehicle', 3)->references('id_vehicle')->on('vehicles')->onDelete('cascade');;
-            $table->foreignId('id_agency', 3)->references('id_agency')->on('agencies')->onDelete('cascade');;
+            $table->foreignId('id_member', 3)->references('id_member')->on('members')->onDelete('cascade');
+            $table->foreignId('id_vehicle', 3)->references('id_vehicle')->on('vehicles')->onDelete('cascade');
+            $table->foreignId('id_agency', 3)->references('id_agency')->on('agencies')->onDelete('cascade');
             $table->dateTime('begin_date_time');
             $table->dateTime('end_date_time');
             $table->integer('Total Price');
